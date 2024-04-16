1bb4cceeef2ec23e7f9b3727e88846b8abd82633
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-16 16:35:53 +0330

    nativewind config

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Stack } from 'expo-router/stack';
+import { Stack } from 'expo-router';
 
 const RootLayout = () => {
   return (
--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Ali</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className='text-3xl'>Ali</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         Go to profile
@@ -13,12 +13,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
