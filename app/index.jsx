c37109d45d3b44be222c585febc0879fb45b77c5
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-17 00:33:27 +0330

    adding button component

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
 export default function App() {
