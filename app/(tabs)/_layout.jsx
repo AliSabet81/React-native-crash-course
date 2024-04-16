8da442421c316e04cbc0dc0c21c381f10f5cbebc
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-16 23:27:09 +0330

    adding all routes (screens)

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { View } from "react-native";
+
+const TabsLayout = () => {
+  return <View></View>;
+};
+export default TabsLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -5,10 +5,10 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className='text-3xl'>Ali</Text>
+      <Text className="text-3xl font-pblack">Ali</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: "blue" }}>
-        Go to profile
+      <Link href="/home" style={{ color: "blue" }}>
+        Go to Home
       </Link>
     </View>
   );
