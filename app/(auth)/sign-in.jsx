91e0673ad225bfae36c7dfe091acfeabe25399f3
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-17 09:33:14 +0330

    changing rootlayout

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -17,7 +17,7 @@ const SignIn = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center h-full px-4 my-6">
+        <View className="w-full justify-center min-h-[81vh] px-4 my-6">
           <Image
             source={images.logo}
             resizeMode="contain"
