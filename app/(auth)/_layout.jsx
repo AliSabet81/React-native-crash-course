8fd053943d7900ff5d855174f7299d0da4ec4919
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-17 09:03:56 +0330

    auth layout

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,7 +1,16 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
 
 const AuthLayout = () => {
-  return <View></View>;
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
+  );
 };
 export default AuthLayout;
