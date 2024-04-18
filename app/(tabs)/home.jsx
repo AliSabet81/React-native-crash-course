523b47a344107aeba87b6a01cd758e0d7291ab9b
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-18 12:13:43 +0330

    trendings

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
+import Trending from "../../components/Trending";
 const Home = () => {
   return (
     <SafeAreaView className="bg-primary">
@@ -31,7 +32,13 @@ const Home = () => {
                 />
               </View>
             </View>
-            <SearchInput/>
+            <SearchInput />
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-gray-100 text-lg font-pregular mb-3">
+                Latest Video
+              </Text>
+              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+            </View>
           </View>
         )}
       />
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -16,7 +16,7 @@ const SearchInput = ({
       <TextInput
         className="flex-1 text-white font-pregular text-base mt-0.5"
         value={value}
-        placeholder={placeholder}
+        placeholder="Search for a video topic"
         placeholderTextColor="#7b7b8b"
         onChangeText={handleChangeText}
         secureTextEntry={title === "Password" && !showPassword}
