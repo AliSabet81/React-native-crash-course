0d05fcf5decc54c10a555d83b5f860a5cf53979c
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-18 12:05:27 +0330

    search input

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View, Text, FlatList, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import SearchInput from "../../components/SearchInput";
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <Text className="text-3xl text-white">{item.id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  Ali Sa
+                </Text>
+              </View>
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
+            <SearchInput/>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default Home
+export default Home;
