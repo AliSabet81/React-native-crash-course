f17b3004c30ce6a1cb7c94aeefa14f5843637f31
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-18 22:58:57 +0330

    showing video list in home page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -14,6 +14,7 @@ import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
 import { getAllPost } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
+import VideoCard from "../../components/VideoCard";
 const Home = () => {
   const { data: posts, refetch, isLoading } = useAppwrite(getAllPost);
   const [refreshing, setRefreshing] = useState(false);
@@ -23,16 +24,13 @@ const Home = () => {
     await refetch();
     setRefreshing(false);
   };
-  
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
-        // data={[]}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
-        )}
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
