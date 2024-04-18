388553c04df79b3bb93ffadea27c1b5c6dcfebf0
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-18 12:38:02 +0330

    empty state

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,11 +4,13 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
+import EmptyState from "../../components/EmptyState";
 const Home = () => {
   return (
-    <SafeAreaView className="bg-primary">
+    <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
+        // data={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
+        data={[]}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
           <Text className="text-3xl text-white">{item.id}</Text>
@@ -40,6 +42,12 @@ const Home = () => {
               <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
             </View>
           </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="Be the first one to upload a video"
+          />
         )}
       />
     </SafeAreaView>
