29f3b683e6224fdda5522c6c21d103ed793c286d
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-20 14:55:51 +0330

    search screen

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,13 +1,49 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { View, Text, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import SearchInput from "../../components/SearchInput";
+import EmptyState from "../../components/EmptyState";
+import { searchPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
+import VideoCard from "../../components/VideoCard";
+import { useLocalSearchParams } from "expo-router";
 
 const Search = () => {
   const { query } = useLocalSearchParams();
+  const {
+    data: posts,
+    refetch,
+    isLoading,
+  } = useAppwrite(() => searchPosts(query));
+
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
   return (
     <SafeAreaView className="bg-primary h-full">
-      <Text className="text-3xl text-white">{query}</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4">
+            <Text className="font-pmedium text-sm text-gray-100">
+              Search Results
+            </Text>
+            <Text className="text-2xl font-psemibold text-white">{query}</Text>
+            <View className="mt-6 mb-8">
+              <SearchInput initialQuery={query} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No video found for this search query"
+          />
+        )}
+      />
     </SafeAreaView>
   );
 };
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -10,10 +10,10 @@ import React, { useState } from "react";
 import { icons } from "../constants";
 import { router, usePathname } from "expo-router";
 
-const SearchInput = () => {
+const SearchInput = ({ initialQuery }) => {
   const pathname = usePathname();
 
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState(initialQuery ? initialQuery : "");
   return (
     <View className="w-full h-16 px-4 bg-black-100 border-2 border-black-200 rounded-2xl focus:border-secondary items-center flex-row space-x-4">
       <TextInput
